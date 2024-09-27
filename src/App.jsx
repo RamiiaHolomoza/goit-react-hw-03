3730f0e232f3efd2d67d473b7fc04418bca7ea39
RamiiaHolomoza <162986900+RamiiaHolomoza@users.noreply.github.com>
2024-09-27 12:59:30 +0300

    change css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useId } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
